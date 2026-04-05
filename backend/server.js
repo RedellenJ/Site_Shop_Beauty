467cc176b1faec4555f5d637b6438ff7cabc4567
EmmanuelAndrade4 <emmanuelcrv1@gmail.com>
2026-04-04 22:54:47 -0300

    feat: conexão com o bd e criação da rota /produtos

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
+require('dotenv').config()
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
   res.json({ mensagem: 'Servidor rodando!' })
+})
+
+app.get('/produtos', async (req, res) => {
+  const supabase = require('./supabase')
+  const { data, error } = await supabase
+    .from('produtos')
+    .select('*')
+
+  if (error) return res.status(500).json({ erro: error.message })
+  res.json(data)
 })
 
 app.listen(PORT, () => {
