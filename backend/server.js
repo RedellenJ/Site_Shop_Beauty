d9fe3da7b2683b16674b90c304a32667db4f2578
EmmanuelAndrade4 <emmanuelcrv1@gmail.com>
2026-04-05 21:08:21 -0300

    feat: criação das rotas produtos e filtroProduto

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 require('dotenv').config()
+const supabase = require('./supabase')
 
 app.use(express.json())
 
@@ -10,12 +11,23 @@ app.get('/', (req, res) => {
 })
 
 app.get('/produtos', async (req, res) => {
-  const supabase = require('./supabase')
   const { data, error } = await supabase
     .from('produtos')
     .select('*')
 
   if (error) return res.status(500).json({ erro: error.message })
+  res.json(data)
+})
+
+app.get('/filtroProduto', async (req, res) => {
+  const { marca } = req.query
+
+  const { data, error } = await supabase
+    .from('produtos')
+    .select('*')
+    .eq('marca', marca)
+
+  if (error) return res.status(500).json({erro: error.message })
   res.json(data)
 })
 
